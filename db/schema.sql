82ad02384aaeb759d45ef98665b39c1b0131dd6f
Dayne Lalmond <dlalmondo@gmail.com>
2022-09-26 09:01:09 -0400

    started schema

---
+++ db/schema.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE team_db;
+
+USE team_db;
+
+CREATE TABLE department (
+    id INT NOT NULL 
+)
+
+CREATE TABLE role (
+    id INT NOT NULL
+)
+
+CREATE TABLE employee (
+    id INT NOT NULL
+)
+
+
+-- Has to be a primary key(id) to autoincrement
