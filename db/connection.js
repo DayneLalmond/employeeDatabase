191f1ce772884cdb04e2ef15eefb45a6a330dc0b
Dayne Lalmond <dlalmondo@gmail.com>
2022-09-28 22:08:31 -0400

    created prompts

---
+++ db/connection.js
@@ -0,0 +1,12 @@
+const mysql = require('mysql2')
+
+
+const server = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "admin",
+    port: 3306,
+    database: "module12"
+  });
+
+  module.exports = server
--- index.js
+++ index.js
@@ -2,16 +2,9 @@
 const inquirer = require('inquirer');
 const chalk = require('chalk');
 const mysql = require('mysql2');
+const server = require('./db/connection');
 const cTable = require("console.table");
 
-
-const server = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "admin",
-  port: 3306,
-  database: "module12"
-});
 
 // connects to mysql database
 server.connect(function (err) {
@@ -23,47 +16,68 @@ server.connect(function (err) {
 });
 
 
-// inquirer.prompt(questions, (answers) => {
-//   console.log(answers);
-// });
-
 function log() {
   inquirer.prompt([
     {
       type: "list",
       name: "command",
-      prefix: chalk.whiteBright('❱'),
-      message: chalk.whiteBright('Select an option below.'),
+      message: 'Select an option below.',
       choices: ["View", "Insert", "Update"]
     },
   ])
     .then((res) => {
       console.log(res)
+      if (res.command === 'View') {
+        console.log('View')
+        display()
+
+      } else if (res.command === 'Insert') {
+        console.log('Insert')
+        addToDepartment()
+
+      } else if (res.command === 'Update') {
+        console.log('Update')
+        selectRole()
+      }
     })
+
 }
 
+function addToDepartment() {
+  inquirer.prompt([{
+    type: "input",
+    name: "deptname",
+    message: ["View", "Insert", "Update"]
+  }]
+  )
+    .then(function (word) {
+      server.query(`INSERT INTO department (name) VALUES ('${word.deptname}')`, function (err) {
+        if (err) throw err
+        console.log("added to dept")
+        log()
+      })
+    })
+    
+}
 
+function selectRole() {
+  inquirer.prompt([{
+    type: "list",
+    name: "role",
+    message: 'Select role',
+    choices: ['Employees', 'That', 'One', 'Two']
+  }])
+  .then(function selectedRole(list) {
+    server.query(`SELECT ${list.role}`)
+  })
+};
 
+function display() {
+  server.query('SELECT * FROM module12.department', function (err, results) {
+    
+    if (err) throw err
+    console.log(results)
+    log()
+  })
 
-
-
-
-
-
-
-
-
-// connect to the database
-// const db = mysql.createConnection(
-//     {
-//         host: 'localhost',
-//         user: 'root',
-//         password: 'admin',
-//         database: 'team_db'
-//     },
-//     console.log(`Connected to team_db database`)
-// )
-
-
-
-// db.query('SELECT * from roles')
+}
